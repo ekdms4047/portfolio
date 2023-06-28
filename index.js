b205638f5197bd1a51705612f4420665c2074f74
ekdms4047 <dh5ek@naver.com>
2023-06-28 16:15:49 +0900

    글씨순서바꾸기효과

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const elements = document.getElementsByClassName('mixing')
+console.log(elements);
 
+
+ for(let i= 0; i < elements.length; i++){
+  const element = elements[i]
+  const text = element.innerHTML
+  element.addEventListener('mouseover',function(){
+  let step = 10
+  const interval = setInterval(function() {
+  const mixed = mixstr(text)
+   element.innerHTML = mixed
+   step = step -1
+   if(step === 0){
+    element.innerHTML = text
+    clearInterval(interval)
+   }
+}, 90);})
+ }
+
+function mixstr(str){
+  const array = str.split('');
+  const arrayLength = array.length;
+  for (let i = arrayLength -1; i>=0; i--) {
+  const randomIndex = Math.floor(Math.random() * (i + 1));
+  [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }  
+  const mixed = array.join('')
+  return mixed
+}
